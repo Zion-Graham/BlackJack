e2d3dc25380879c75b53adc9d54f3e82d33b16a2
Zion Graham <ziongraham96@gmail.com>
2018-09-01 18:23:45 -0500

    Created hit() and getHandValue() methods in Player.

--- app/src/main/java/com/example/ziong/blackjack/Player.java
+++ app/src/main/java/com/example/ziong/blackjack/Player.java
@@ -1,10 +1,24 @@
 package com.example.ziong.blackjack;
 
+import java.util.ArrayList;
+
 /*
  * This class represents the Player in BlackJack
- * Each Player is able to do one of 3 actions Deal, Hit, Stay
+ * Each Player is able to do one of 5 actions Deal, Hit, Stay, Double, Split
+ * Each player has a "hand", a deck of cards in his/her hand
  */
 public class Player
 {
+   private Hand playerHand = new Hand();
 
+   public void hit(Card card)
+   {
+       playerHand.addCard(card);
+   }
+
+   // Prints the value of the player's hand
+   public void getHandValue()
+   {
+       playerHand.getValue();
+   }
 }
